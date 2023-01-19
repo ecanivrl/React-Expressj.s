166897b8f42b43ddbe46cc07af01fff8933719b7
Ercan <kadri503394@gmail.com>
2023-01-19 15:46:32 +0300

    feat: added  user information api and  table

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
 import "./home.css"
 
 export const Home = () => {
+    const [data, setData] = useState([])
+   
+
+    useEffect(() => {
+          getUser()
+    }, [])
+
+    const getUser = async () => {
+        const res = await axios.get("http://localhost:5000/users")
+        if(res.status === 200) {
+            setData(res.data)
+        }
+    }
+
   return (
     <div className="table-wrapper">
         <table>
@@ -10,19 +26,22 @@ export const Home = () => {
                     <th>Name</th>
                     <th>Email</th>
                     <th>Country</th>
+                    <th>City</th>
                     <th>Contact</th>
                     <th>Action</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Ecani</td>
-                    <td>Ecani@gmail.com</td>
-                    <td>Turkey</td>
-                    <td>0543 801 48 28</td>
-                    <td>Edit</td>
-                </tr>
+               {data && data.map ((user, index) => (
+                 <tr key={user.id}>
+                 <td>{index + 1}</td>
+                 <td>{user.name}</td>
+                 <td>{user.email}</td>
+                 <td>{user.country}</td>
+                 <td>{user.city}</td>
+                 <td>{user.contact}</td>
+             </tr>
+               ))}
             </tbody>
         </table>
     </div>
