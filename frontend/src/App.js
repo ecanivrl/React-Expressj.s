5c73600e821c779df94b4dbf182a4516aaf18d4d
Ercan <kadri503394@gmail.com>
2023-01-20 17:10:45 +0300

    feat: added react toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,15 @@ import { Header } from './components/Header';
 import { AddEdit } from './pages/addedit/AddEdit';
 import { Home } from './pages/home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function App() {
   return (
     <Router>
       <Header />
+      <ToastContainer />
       <div className="container">
       <Routes>
         <Route path="/" element={<Home />} />
--- frontend/src/pages/addedit/AddEdit.jsx
+++ frontend/src/pages/addedit/AddEdit.jsx
@@ -1,110 +1,8 @@
-// import axios from "axios";
-// import "./addedit.css"
-// import React, { useState, useEffect } from "react";
-
-// const initialState = {
-//     name: "",
-//     email: "",
-//     country: "",
-//     city: "",
-//     contact: "",
-// };
-
-// export const AddEdit = () => {
-//     const [data, setData] = useState(initialState);
-//     const { name, email, country, contact, city } = data;
-
-//     const createUser = async (data) => {
-//         const res = await axios.post("http://localhost:5000/users/", data);
-//         if (res.status === 200) {
-//         }
-//     };
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         if (!name || !email || !country || !contact || !city) {
-//             return;
-//         }
-
-//     };
-
-//     const handleInputChange = (e) => {
-//         const { name, value } = e.target;
-//         setData({ ...data, [name]: value });
-//     };
-
-//     return (
-//         <div>
-//             <form onSubmit={handleSubmit}>
-//                 <div className="input-wrapper">
-//                     <label htmlFor="name">Name</label>
-//                     <input
-//                         type="text"
-//                         id="name"
-//                         name="name"
-//                         placeholder="Enter a name."
-//                         onChange={handleInputChange}
-//                         value={name}
-//                     />
-//                 </div>
-//                 <div className="input-wrapper">
-//                     <label htmlFor="email">Email</label>
-//                     <input
-//                         type="email"
-//                         id="email"
-//                         name="email"
-//                         placeholder="Enter a email."
-//                         onChange={handleInputChange}
-//                         value={email}
-//                     />
-//                 </div>
-//                 <div className="input-wrapper">
-//                     <label htmlFor="country">Country</label>
-//                     <input
-//                         type="text"
-//                         id="country"
-//                         name="country"
-//                         placeholder="Enter a country."
-//                         onChange={handleInputChange}
-//                         value={country}
-//                     />
-//                 </div>
-//                 <div className="input-wrapper">
-//                     <label htmlFor="country">City</label>
-//                     <input
-//                         type="text"
-//                         id="city"
-//                         name="city"
-//                         placeholder="Enter a city."
-//                         onChange={handleInputChange}
-//                         value={city}
-//                     />
-//                 </div>
-//                 <div className="input-wrapper">
-//                     <label htmlFor="contact">Contact</label>
-//                     <input
-//                         type="text"
-//                         id="contact"
-//                         name="contact"
-//                         placeholder="Enter a contact."
-//                         onChange={handleInputChange}
-//                         value={contact}
-//                     />
-//                 </div>
-//                 <input
-//                     type="submit"
-//                     className="btn btn-success"
-//                 />
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default AddEdit;
-
 import React,{useState} from 'react';
 import './addedit.css';
 import axios from 'axios';  
+import { toast } from "react-toastify";
+
 
 const initialState = {
     name: '',
@@ -124,15 +22,14 @@ export const AddEdit = () => {
     const createUser = async (data) => {
         const res = await axios.post("http://localhost:5000/users/", data);
         if(res.status === 200){
-            window.confirm(res.data)
-            // window.confirm("User Created Successfully")
+          toast.success("User Created Successfully")
         }
     }
 
 const handleSubmit = (e) => {
     e.preventDefault();
     if(!name || !email || !country || !city || !contact){
-        window.confirm("Please fill all the fields")
+        toast.error("Please fill all the fields")
         return;
     };
     createUser(data);
@@ -141,6 +38,7 @@ const handleSubmit = (e) => {
 const handleInputChange = (e) => {
     const {name, value} = e.target;
     setData({...data, [name]: value})
+    console.log(data)
 }
   return (
     <div>
